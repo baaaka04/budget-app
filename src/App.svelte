7697ebfc4623eb3a0fc1e9120e84fbdb519d4e6b
ab <ab@sdflkj.csm>
2022-04-23 16:52:04 +0300

    add navigate panel on bottom

--- src/App.svelte
+++ src/App.svelte
@@ -6,18 +6,47 @@
 </script>
 
 <div>
-    <button type="button" on:click={() => page = "home"}>Home</button>
-    <button type="button" on:click={() => page = "pie" }>PieChart</button>
-
     {#if page === "home"}
         <Table />
         <Form />
     {:else if page === "pie"}
         <p>Ну типа</p>
     {/if}
+
+    <div class="nav-panel">
+        <img
+            src="src/icons/home.svg"
+            alt=""
+            on:click={() => {
+                page = "home";
+            }}
+        />
+        <img
+            src="src/icons/piechart.svg"
+            alt=""
+            on:click={() => {
+                page = "pie";
+            }}
+        />
+    </div>
 </div>
 
 <style>
+    .nav-panel {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        position: fixed;
+        background-color: rgb(45, 45, 54);
+        width: 100%;
+    }
+
+    .nav-panel img {
+        margin: 10px 20px 10px 20px;
+        width: 40px;
+    }
+
     :root {
         color: #eee;
         background-color: rgb(39, 39, 42);
