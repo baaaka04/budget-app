f62aa2dab23e5782c204bdbc3034be86baf0bc12
ab <ab@sdflkj.csm>
2022-06-19 17:25:00 +0300

    feat: add authenticate

---
+++ src/AuthWrapper.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Login from "./Login.svelte";
+    const _user = "YWI6d2hhdA==";
+    const user = localStorage.getItem("user");
+</script>
+
+<div>
+    {#if _user == user}
+        <slot />
+    {:else}
+        <Login />
+    {/if}
+</div>
